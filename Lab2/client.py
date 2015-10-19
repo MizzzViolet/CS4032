fc19f49bfb4566906656ea1d814c6efe0d6e550e
Jasmine <jasmine.tlkdr@gmail.com>
2015-10-19 22:28:40 +0100

    Added Threads

--- Lab2/client.py
+++ Lab2/client.py
@@ -1,19 +1,26 @@
-import socket
+from socket import *
 import thread
 import sys
 import time
-def handler(host, port, message):
 
+#def handler(HOST, PORT, message):
+def handler():
+    HOST = ''
+    PORT = 8002
     tcpSocket = socket(AF_INET, SOCK_STREAM)
     tcpSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    tcpSocket.connect((host, port))
-    tcpSocket.sendall(msg + "\n")
-    data = tcpSocket.recv(1024)
+    tcpSocket.connect((HOST, PORT))
+    while True:
+        message = raw_input("Message: ")
+        tcpSocket.send(message)
+        data = tcpSocket.recv(1024)
+        print "Received: " + repr(data)
     tcpSocket.close()
-    return data
 
-print handler
+
 if __name__ == '__main__':
-    # for x in range(0,5):
-        t = thread.start_new(handler, (sys.argv[1], int(sys.argv[2]), sys.argv[3]))
-# time.sleep(10)
+    handler()
+#    #input host, port number, message
+#    for x in range(0,5):
+#        t = thread.start_new(handler, (sys.argv[1], int(sys.argv[2]), sys.argv[3]))
+#    time.sleep(10)
--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,37 +1,51 @@
-#python 2 !!!
-
 from socket import *
 import thread
 
-def connect(clientSocket, clientAddress):
+def response(key):
+    return "Server received: " + key
 
-    print "Connection from ", clientAddress
-    while 1:
-        data = tcpSocket.recv(1024)
+def hello_res(clientAddress, PORT):
+    s = "HELO text\nIP:" + repr(clientAddress) + "\nPORT:" + str(PORT)
+    return s
 
-    print "\nData :" + data
-    if not data:
-        break
-    else:
-        message = "\nMessage sent: %s" % data
-        clientsocket.send(message)
-    print "\nConnection ended.\n"
+def handler(clientSocket, clientAddress, PORT):
+
+    while True:
+        data = clientSocket.recv(1024)
+        if not data: break
+        if "KILL_SERVICE" == data.rstrip():
+            break
+        if "HELO text" == data.rstrip():
+            clientSocket.sendall(hello_res(clientAddress, PORT))
+        else:
+            print repr(clientAddress) + " Received: " + repr(data)
+            clientSocket.sendall(response(data))
+            print repr(clientAddress) + " Sent: " + repr(response(data))
+
+    print "\nConnection ended: " + clientAddress
     clientSocket.close()
+
 
 
 if __name__ == "__main__":
 
     tcpSocket = socket(AF_INET, SOCK_STREAM)
     tcpSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    host = "0.0.0.0"
-    port = 8000
-    tcpSocket.bind((host, port))
-    tcpSocket.listen(5)
-
-    while 1:
-
-        print "Waiting for connections\n"
-        clientSocket, address = tcpSocket.accept()
-        thread.start_new_thread(connect, (clientSocket, address))
-
+    HOST = ''
+    PORT = 8002
+    tcpSocket.bind((HOST, PORT))
+    tcpSocket.listen(100)
+    print "Waiting for connection..."
+    
+    while True:
+        try:
+            clientSocket, address = tcpSocket.accept()
+            print "Connection from ", address
+            thread.start_new_thread(handler, (clientSocket, address, PORT))
+        except KeyboardInterrupt:
+            print "KILLED."
+            break
+        except socket.error, msg:
+            print "Socket error! %s" % msg
+            break
     tcpSocket.close()
