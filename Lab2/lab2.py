8dd93577f43bf2f27c5c8b685e443fc70c1fb0ba
Jasmine <jasmine.tlkdr@gmail.com>
2015-10-25 21:30:25 +0000

    kill fixed

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,14 +1,16 @@
 from socket import *
 from threading import Thread
 from Queue import Queue
-
-PORT = 8002
+import sys, os, signal
 
 def response(key):
-    return "Server received: " + key
+    return "Server received: %s \n" % key
 
-def hello_res(clientAddress, PORT):
-    s = "HELO text\nIP:" + repr(clientAddress) + "\nPORT:" + str(PORT)
+def kill():
+    return "Connection ended. \n"
+
+def hello_res(data, clientAddress, PORT):
+    s = data + "\nIP: " + repr(clientAddress) + "\nPORT: " + str(PORT) + "StudentID: b56be41b6c15446d8ea27c05d121f0324c88a2beed698357e197527d43112b48 \n"
     return s
 
 def client(worker):
@@ -17,29 +19,37 @@ def client(worker):
         
         clientSocket = worker.get()
         clientAddress = worker.get()
-        
-        while True:
+        dead = False
+
+        while not dead:
+            
             data = clientSocket.recv(1024)
             if not data: break
-            if "KILL_SERVICE" == data.rstrip():
-                break
-            if "HELO text" == data.rstrip():
-                clientSocket.sendall(hello_res(clientAddress, PORT))
+            if data == "KILL_SERVICE":
+                clientSocket.sendall(kill())
+                dead = True
+            elif data[:4] == "HELO":
+                print repr(clientAddress) + " Received: " + repr(data)
+                clientSocket.sendall(hello_res(data,clientAddress, PORT))
+                print repr(clientAddress) + " Sent: " + repr(hello_res(data,clientAddress, PORT))
             else:
                 print repr(clientAddress) + " Received: " + repr(data)
                 clientSocket.sendall(response(data))
                 print repr(clientAddress) + " Sent: " + repr(response(data))
-  
-    print "\nConnection ended: " + clientAddress
+            
+        if dead:
+            print "\nConnection ended."
+            os.kill(os.getpid(), signal.SIGINT)
+                
     clientSocket.close()
 
-def handler(HOST, workers):
+def handler(HOST, PORT,workers):
     
     tcpSocket = socket(AF_INET, SOCK_STREAM)
     tcpSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     tcpSocket.bind((HOST, PORT))
     tcpSocket.listen(100)
-
+    print "Waiting for connections..."
     q = Queue(workers)
     
     for x in range(workers):
@@ -60,8 +70,8 @@ def handler(HOST, workers):
             print "Socket error! %s" % msg
             break
 
-
 if __name__ == "__main__":
-
-     handler("",10)
+    if len(sys.argv) == 3:
+        handler("0.0.0.0", int(sys.argv[1]), int(sys.argv[2]))
+    else:   print "Format needed: python " + sys.argv[0] + " PORT THREADS"
 
