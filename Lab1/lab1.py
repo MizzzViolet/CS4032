18fcce8d7c5bb68390f15d7c5d2aef5f66f0300d
Jasmine <jasmine.tlkdr@gmail.com>
2015-10-25 20:25:54 +0000

    User has to input the host, port number, and message when running the program

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,15 +1,20 @@
 import socket
+import sys
 
-def connect():
-    
+def connect(HOST, PORT, msg):
+
     tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = tcpSocket.connect(('0.0.0.0',8001))
-
-    message = raw_input()
-    tcpSocket.sendall("GET http://0.0.0.0/echo.php?message=%s HTTP/1.0\r\n\r\n" % message)
-
+    tcpSocket.connect((HOST, PORT))
+    tcpSocket.sendall("GET /echo.php?message=%s HTTP/1.0\r\n\r\n" % msg)
     data = tcpSocket.recv(1024)
     tcpSocket.close()
     return data
 
-print connect()
+# User has to input the host, port number, and message when running the program
+if __name__ == "__main__":
+
+    if len(sys.argv) == 4:
+        print connect(sys.argv[1], int(sys.argv[2]), sys.argv[3]) + "\n"
+    else:
+        print "Invalid Input. Format needed: python " + sys.argv[0] + " HOST PORT Message\n"
+
